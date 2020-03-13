b307c2792382534c83fbb74c34570f906d66dc93
offa <bm-dev@yandex.com>
2020-03-13 12:17:05 +0100

    ManagementLink added.

---
+++ src/main/java/bv/offa/jenkins/maintenance/MaintenanceModeLink.java
@@ -0,0 +1,79 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 offa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package bv.offa.jenkins.maintenance;
+
+import hudson.Extension;
+import hudson.model.ManagementLink;
+import hudson.util.FormApply;
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.interceptor.RequirePOST;
+
+import javax.annotation.CheckForNull;
+
+@Extension
+public class MaintenanceModeLink extends ManagementLink
+{
+    private volatile boolean active;
+
+    @CheckForNull
+    @Override
+    public String getIconFileName()
+    {
+        return "gear2.png";
+    }
+
+    @CheckForNull
+    @Override
+    public String getDisplayName()
+    {
+        return "Maintenance Mode";
+    }
+
+    @CheckForNull
+    @Override
+    public String getUrlName()
+    {
+        return "maintenance-mode";
+    }
+
+    @Override
+    public boolean getRequiresPOST()
+    {
+        return true;
+    }
+
+    public boolean isActive()
+    {
+        return active;
+    }
+
+    @RequirePOST
+    public HttpResponse doToggleMode(StaplerRequest req)
+    {
+        active = !active;
+        return FormApply.success(".");
+    }
+}
---
+++ src/test/java/bv/offa/jenkins/maintenance/MaintenanceModeLinkTest.java
@@ -0,0 +1,59 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 offa
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package bv.offa.jenkins.maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.kohsuke.stapler.StaplerRequest;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+
+class MaintenanceModeLinkTest
+{
+    @Test
+    void postRequired()
+    {
+        final MaintenanceModeLink link = new MaintenanceModeLink();
+        assertThat(link.getRequiresPOST()).isTrue();
+    }
+
+    @Test
+    void isInactiveByDefault()
+    {
+        final MaintenanceModeLink link = new MaintenanceModeLink();
+        assertThat(link.isActive()).isFalse();
+    }
+
+    @Test
+    void toggleChangesState()
+    {
+        final MaintenanceModeLink link = new MaintenanceModeLink();
+        final StaplerRequest req = mock(StaplerRequest.class);
+        assertThat(link.isActive()).isFalse();
+        link.doToggleMode(req);
+        assertThat(link.isActive()).isTrue();
+    }
+
+}
