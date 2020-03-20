df734844e2c664257dd7051c21b79db0abef6de4
offa <bm-dev@yandex.com>
2020-03-20 12:02:48 +0100

    Disable maven transfer progress in ci builds.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
-mvn verify -B
+mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V --no-transfer-progress
+mvn verify -B --no-transfer-progress
